b81dda65ce7d3fbf03dc92f7bff401b809ac86e2
林晃人 <hayashi@hayashiakiraninnoMacBook-Pro.local>
2020-01-19 14:51:08 +0900

    first coomit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+
+def new
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+      book = Book.new(book_params)
+      book.save
+      redirect_to book_path(book.id)
+    end
+
+  def index
+    @books = Book.all
+    @book =Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully created"
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_new_path
+    flash[:notice] = "Book was successfully destroyed"
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
---
+++ db/migrate/20200118111842_drop_book.rb
@@ -0,0 +1,5 @@
+class DropBook < ActiveRecord::Migration[5.2]
+  def change
+  	drop_table :books
+  end
+end
